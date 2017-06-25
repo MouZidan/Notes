f56f78b63c2ce1445c69d23954a70f03c6049d1e
mr-mou <moh.anad23823@gmail.com>
2017-06-26 01:28:09 +0200

    Fixing

--- app/src/main/java/com/mou/inc/myapplication/RecorderActivity.java
+++ app/src/main/java/com/mou/inc/myapplication/RecorderActivity.java
@@ -41,8 +41,6 @@ public class RecorderActivity extends AppCompatActivity {
 
 
 
-
-
         ImageButton recordBtn=(ImageButton)findViewById(R.id.recrod_button);
         recordBtn.setOnClickListener(new View.OnClickListener() {
             @Override
